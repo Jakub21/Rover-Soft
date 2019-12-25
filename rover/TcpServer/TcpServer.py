dfe1dd435cc0c4eeb94e244fb68fd3daed3e0533
Jakub21 <jakub21@mail.com>
2019-12-25 17:44:17 +0100

    Connection plugins update 6

--- rover/TcpServer/TcpServer.py
+++ rover/TcpServer/TcpServer.py
@@ -13,7 +13,7 @@ class TcpServer(Plugin):
   def initSocket(self):
     self.socket = scklib.socket()
     Note(self, f'Binding TPC server to {self.ownAddress}:{self.usedPort}')
-    self.socket.setsockopt(scklib.SO_REUSEADDR)
+    self.socket.setsockopt(scklib.SOL_SOCKET, scklib.SO_REUSEADDR, 1)
     self.socket.bind((self.ownAddress, self.usedPort))
     self.socket.settimeout(self.cnf.timeOut)
     self.connection = Namespace(state=False, socket=None, address='', port=0)
