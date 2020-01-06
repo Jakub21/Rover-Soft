5e634c6fac61a454b04d646740b04b0bf39eeab1
Jakub21 <jakub21@mail.com>
2020-01-06 09:24:40 +0100

    Interface repair

--- universal/Interface/Scope.py
+++ universal/Interface/Scope.py
@@ -1,6 +1,7 @@
 import TkiWrapper as tki
-from TkiWrapper import widgets as tkw
-from TkiWrapper import canvas as tkc, canvas.elements as tkce
 from tkinter import TclError
+from TkiWrapper import widgets as tkw
+from TkiWrapper import canvas as tkc
+tkce = tkc.elements
 
 from os import name as systemName
