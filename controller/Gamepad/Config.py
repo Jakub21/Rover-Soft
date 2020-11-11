32b6b3dfb6848d21822d549a17449114cf5146e3
Jakub21 <jakubp2101@gmail.com>
2020-11-11 12:13:59 +0100

    Update gamepad config

--- controller/Gamepad/Config.py
+++ controller/Gamepad/Config.py
@@ -4,13 +4,14 @@ class Config:
   gamepadIndex = 0
 
   # Specification of connected gamepad
-  class gpSpecs:
-    buttons = 10; axes = 5; hats = 1; balls = 0
+  gpSpecs = Namespace( # [PLAYSTATION 3 GAMEPAD WITH SCP]
+    buttons = 10, axes = 5, hats = 1, balls = 0
+  )
 
   # Name controls (different order/names for each Gamepad model)
   # Each name must be unique
 
-  # [PLAYSTATION 3 GAMEPAD]
+  # [PLAYSTATION 3 GAMEPAD WITH SCP]
   keysButtons = [
     'Down', 'Right', 'Left', 'Up', # Right-hand buttons with symbols
     'BackLeft', 'BackRight', # Back panel
